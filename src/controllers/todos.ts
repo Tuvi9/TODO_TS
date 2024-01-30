c4a3d46240eb1727e0a8e7f5a946b15583abe16a
Kaspar <kaspupavel@gmail.com>
2024-01-30 17:49:14 +0200

    POST works. Made sure by POSTMAN

---
+++ src/controllers/todos.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { Todo } from '../models/todo';
+
+const todos: Todo[] = [];
+
+export const createTodo = (req: Request, res: Response, next: NextFunction) => {
+    try{
+        // POST Request body: {task: string}
+        const task = (req.body as {task: string}).task
+        const newTodo = new Todo(Math.random().toString(), task);
+        todos.push(newTodo);
+        res.status(201).json({
+            message: 'Created new todo',
+            createdTask: newTodo
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { createTodo } from "../controllers/todos";
 
 const router = Router();
 
-router.post('/')
+// Handle routes here
+router.post('/', createTodo)
 router.get('/')
 router.patch('/:id')
 router.delete('/:id')
